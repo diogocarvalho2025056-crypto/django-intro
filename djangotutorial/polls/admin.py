552d14f5f313f4868a77a64d9d3af841e9b056f3
Diogo <diogo.carvalho.2025056@my.istec.pt>
2026-04-14 11:21:59 +0100

    tudo

---
+++ djangotutorial/polls/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
+
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+from django.contrib import admin
+
+from .models import Choice, Question
+
+# ...
+admin.site.register(Choice)
+
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceInline(admin.TabularInline): ...
+
+class QuestionAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ["question_text", "pub_date"]
+    
+class QuestionAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ["question_text", "pub_date", "was_published_recently"]
